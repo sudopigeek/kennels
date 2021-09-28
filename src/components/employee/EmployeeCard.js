7d22faba62159bf790ba1baa9c8fd786ad8f2750
sudopigeek <samuelcatferrell@gmail.com>
2021-09-28 09:20:51 -0700

    feat(kennels-firstcommit)

---
+++ src/components/employee/EmployeeCard.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Employee.css"
+
+export const EmployeeCard = () => (
+    <section className="employee">
+        <h3 className="employee__name">Jacob Wyatt</h3>
+        <div className="employee__location">Nashville Kennels North</div>
+    </section>
+)
